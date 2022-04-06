259eb5d424ddd9fd36c175a4a0b92bdb63408460
davisap2 <davisap1@udmercy.edu>
2022-04-05 22:02:13 -0400

    added method import function (incomplete)

--- pyunittest.py
+++ pyunittest.py
@@ -4,9 +4,10 @@ Created on Sat Mar 26 15:41:27 2022
 
 @author: Austin Davis
 """
-import sys
+import os, sys
 
 from pyunitast.ast import parseAST
+import tests.test1
 
 class UnitTest:
     def __init__(self, className):
@@ -22,24 +23,56 @@ class UnitTest:
     def printFunctions(self):
         print(self.functions)
         
+    def runMethod(self, methodName):
+        #run the given method here
+        print("method Name: ", methodName+'()')
+        eval(methodName+'()')
+        
+    def importClasses(self):
+        testPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tests\\') # directory of test suites
+        print("path: ", testPath)
+        print("__name__: ", __name__)
+        
+        # for each file in the tests folder, import the class inside them
+        for py in [f[:-3] for f in os.listdir(testPath) if f.endswith('.py') and f != '__init__.py']:
+            print("py: ", py)
+            #mod = __import__('.'.join([__name__, py]), fromlist=[py])
+            
+            #mod = __import__('tests.'+py)
+            mod = __import__('.'.join(['tests', py]), fromlist=[py])
+            print("imported ", 'tests.'+py)
+            
+            for x in dir(mod):
+                if isinstance(getattr(mod, x), type):
+                    print("x: ", x)
+            
+            classes = [getattr(mod, x) for x in dir(mod) if isinstance(getattr(mod, x), type)]
+            print("classes: ", classes)
+            for cls in classes:
+                print("a cls")
+                setattr(sys.modules[__name__], cls.__name__, cls)
+        
+        
     def runTests(self):
         setupFunctions = self.functions['setup']
         teardownFunctions = self.functions['teardown']
         testFunctions = self.functions['test']
         
+        self.importClasses()
+        
         #beforeClass(setupFunctions)
         
         for test in testFunctions:
-            beforeEach()
-            passOrFail = runThisTest(test)
-            #eval(self.testClass.test+'()') # runs the given function
-            pass
+            print(test)
+            #beforeEach()
+            #passOrFail = runMethod(test)
+            #self.runMethod(test)
+            
+            
         
         #afterClass(teardownFunctions)
     
-    def runMethod(methodName):
-        #run the given method here
-        
+
         
         
 
---
+++ tests/test1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  4 23:21:23 2022
+
+@author: Austin Davis
+"""
+
+class TestSuiteOne:
+    def __init__(self):
+        self.className = self.__class__.__name__
+    def beforeClass():
+        print("This function should run once before any other function")
+    
+    def beforeEach():
+        print("This functions should run before each test function")
+        
+    def testFunc():
+        print("This is a test function")
+        
+    def afterEach():
+        print("This function should run after each test function")
+        
+    def afterClass():
+        print("This function should run after all test functions have run")
